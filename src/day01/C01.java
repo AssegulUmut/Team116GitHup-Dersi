64d07279a79790320bfe042d30244eef4b1d2f16
aysegulUmut <assegul.kotay@gmail.com>
2023-04-26 12:49:55 +0300

    ilk commit

---
+++ src/day01/C01.java
@@ -0,0 +1,18 @@
+package day01;
+
+public class C01 {
+    /*
+    git.init komutu, bir workingDirectory'u git ile ilk atamayi yapar
+    //bu komut her proje icin sadece bir kere yapilir
+    //bu komut ile icinde bulunulan klasöru kontrol etmeye baslar
+    //Bu klasörde yedek almak istedigmizde commit yapmaliyi.
+    //Asagidaki üc komut bizim dosyalarimizi her yedeklemek istedigimizde kullanilir
+
+    1-git add .   :Son kaydedilen versiyondan sonra projede yapilan degisiklikleri belirler
+
+
+
+
+     */
+
+}
