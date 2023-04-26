fadcf3f6833796e176daee76cb6b2d74bb9101a3
aysegulUmut <assegul.kotay@gmail.com>
2023-04-26 12:55:20 +0300

    C01'de aciklamalar eklendi

--- src/day01/C01.java
+++ src/day01/C01.java
@@ -10,6 +10,17 @@ public class C01 {
 
     1-git add .   :Son kaydedilen versiyondan sonra projede yapilan degisiklikleri belirler
 
+    2.git commit -m "commit ismi"  :Projenin son durumunu bir versiyon olarak kaydeder. Burada
+    cift tirnak icinde yazilan Commit isminin anlamli olmasinde fayda vardir. ilerleyen asamalarda geri dönüs
+    yapmak istersek hangi commit'e dönmek isteyecegimizi aciklamalardan bulabiliriz.
+
+    git ile ilisiklendirdigimiz dosyadaki class isimleri git durumuna göre renklenir.
+    kirmizi:bu class herhangi bir versiyona eklenmedi
+    mavi: bu class bir versiyona eklendi ama sonradan degisiklik yapildi
+    siyah:bu class bir versiyonda yedeklendi sonrasinda hicbir degisiklik yapilmadi
+
+    3.
+
 
 
 
