9cfe954f15306b0ff2d792c32aab65bf18cffad6
aysegulUmut <assegul.kotay@gmail.com>
2023-04-28 00:11:51 +0300

    ekleme

--- src/day01/C03.java
+++ src/day01/C03.java
@@ -4,5 +4,31 @@ public class C03 {
     public static void main(String[] args) {
         System.out.println("2.gün ilk class");
         System.out.println("aciklama");
+        /*
+        kisisel kullanimda eski commitlere donmek istedigimizde tek bir git komutu
+        yeterli olur. Ancak islemi yapmadan once commit yaparak son halini git'e gönderebiliriz
+
+       senaryo:eski bir commit'e gidip orada inceleme yaptıktan sonra projenin son haline geri dönün
+        1- projenin son halini commit yapın
+        2- git log --oneline diyerek commit gecmisini görun
+        3- istediginiz commit'e gitmek icin git checkout 123456
+        (istenenn commit'in hash kodunun ilk 7 hanesi)
+        4- istedigimiz commit'i inceledik ve projenin son haline donmek istiyorsak
+        git checkout 1234567(son commit'in hash kodu)
+
+
+        senaryo: son yapilan commit'i gecersiz kıllıp, bir onceki commit'e donun
+
+        bu yazi yanlislikla yazildi, bir onceki commit'e donelim
+
+        not: geriye donme islemlerinde local repo ile remote repo birbirinden farklılasacagı
+        icin geriye dondugumuzde baska islem yapmadan remote repo'yu guncellemek gerekir
+
+        checkout :commitler arasinda gecis yapar(bakip gecmek icin)
+        revert :geriye dondurur ve o halini otomatik olarak commit yapar
+        yani kalici olarak geriye dondurmus olur.
+
+
+         */
     }
 }
---
+++ src/day01/C05_branchKullanimi.java
@@ -0,0 +1,26 @@
+package day01;
+
+public class C05_branchKullanimi {
+    /*
+    bir proje devam ederken deneme amacli projeyi veya
+    asil projeyi bozmadan proje uzerinde brach kullaniriz
+
+    git branch: var olan branch'lerin listesini verir
+    git brach branchAdi:verdigimiz isimde yeni bir branch olusturur
+    git brach branchAdi:verdigimiz isimdeki branch e gecer
+
+    oluısturdugumuz yeni branch te istedigimiz degisiklikleri yaptıktan sonra
+    bu degisiklikleri main branch'e islenmesini istiyorsak
+
+    1.once main branch'e geceriz
+   git checkout main
+
+    2.main branch'de iken deneme branch'indeki degisklikleri kaydederiz
+    git merge deneme
+
+
+    cakisma olmadigi müddetce git merge deneme branch'leri tüm degisiklikleri main
+    brach'e isler
+
+     */
+}
